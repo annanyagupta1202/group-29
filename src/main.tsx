73b87f6a7f24d07cc9b9d9333d093650c3d7d6ac
cyrielleazoyan-cyber <cyrielle.azoyan@edu.escp.eu>
2026-09-11 17:00:03 +0200

    Improve Van Westendorp price scale layout

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './styles.css';
 import './controls.css';
+import './price-scale.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
